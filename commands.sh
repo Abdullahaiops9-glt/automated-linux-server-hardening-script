0ea04d0dd32964ceb5ea571b28bcde6a622eb2f9
Abdullah <abdullahaiops9@gmail.com>
2026-06-15 17:30:51 +0500

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -8,9 +8,7 @@ sudo ./verification.sh
 
 grep PermitRootLogin /etc/ssh/sshd_config
 
-systemctl status firewalld
-
-getenforce
+sudo ufw status verbose
 
 systemctl list-units --type=service --state=running
 
